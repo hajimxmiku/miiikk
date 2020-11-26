10383e01a714c53c2c66a867f80c20ccdb3e2cd1
hajimxmiku <anakhandsomee@gmail.com>
2020-11-27 04:26:02 +0800

    Update miku.py

--- miku.py
+++ miku.py
@@ -82,7 +82,7 @@ while True:
 		print(f"[:)] {g} GB has been successfully added to your account.")
 		print(f"[#] Total: {g} Good {b} Bad")
 		print("[*] After 18 seconds, a new request will be sent.")
-		time.sleep(0.1)
+		time.sleep(1)
 	else:
 		b = b + 1
 		os.system('cls' if os.name == 'nt' else 'clear')
